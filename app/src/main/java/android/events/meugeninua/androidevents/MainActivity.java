5a4a136de0bef584c9eecacf41110e41ec98b159
meugen <evgeniy.m@powercode.us>
2018-05-04 08:44:06 +0300

    Created basic struct for project

---
+++ app/src/main/java/android/events/meugeninua/androidevents/MainActivity.java
@@ -0,0 +1,13 @@
+package android.events.meugeninua.androidevents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
